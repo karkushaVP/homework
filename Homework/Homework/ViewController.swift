3e1d8bd9934b6587f94bb0831edbe92dee096b8b
karkushaVP <144828076+karkushaVP@users.noreply.github.com>
2023-09-13 12:46:10 +0200

    first commit

---
+++ Homework/Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Polya on 13.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
